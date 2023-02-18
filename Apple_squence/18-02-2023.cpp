a48febb70f6a0043923cae8c12c53ab790a06f6a
shakti2002 <109802187+shakti2002@users.noreply.github.com>
2023-02-18 02:04:18 -0800

    Create 18-02-2023.cpp

---
+++ Apple_squence/18-02-2023.cpp
@@ -0,0 +1,32 @@
+class Solution{   
+public:
+    int appleSequences(int n, int m, string arr){
+        // code here 
+        int end=0;
+        int st=0;
+        int ans=0;
+        
+        while(end!=n){
+            if(m>0){
+                if(arr[end]=='O'){
+                    m--;
+                }
+            }
+            else{
+                
+                if(arr[end]=='O'){
+                while(arr[st]!='O'){
+                    st++;
+                }
+                st++;
+                }
+                
+            }
+            ans= max(ans, end-st+1);
+            end++;
+        }
+        return ans;
+        
+        
+    }
+};
